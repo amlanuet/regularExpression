9ebe19208053a96e2477113b1509a152f1625047
amlanuet <34990186+amlanuet@users.noreply.github.com>
2020-11-18 14:36:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
 pattern = re.compile("^(\S+) (\S+) (\S+) \[([\w:\/]+\s[+\-]\d{4})\] \"(\S+)\s?(\S+)?\s?(\S+)?\" (\d{3}|-) (\d+|-)\s?\"?([^\"]*)\"?\s?\"?([^\"]*)\"?\s")
+
 
 for i, line in enumerate(open("/home/teun/Downloads/access.log")):
     for match in re.finditer(pattern, line):
-        print match.group()
+        print 'Found on line %s: %s' % (i+1, match.group()
