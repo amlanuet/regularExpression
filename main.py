608972e93df21b9866ad4281c0e67ce3985cd99c
Teun <teunteun@live.nl>
2020-11-30 13:53:02 +0100

    added database interaction and saves separated part of  the log file into the log s table's columns

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import re
+import pymysql
+
+connection = pymysql.connect("localhost",'user_1','toor',"regulairEx");
+cursor = connection.cursor();
+
+
 pattern = re.compile("^(\S+) (\S+) (\S+) \[([\w:\/]+\s[+\-]\d{4})\] \"(\S+)\s?(\S+)?\s?(\S+)?\" (\d{3}|-) (\d+|-)\s?\"?([^\"]*)\"?\s?\"?([^\"]*)\"?\s")
 ip = []
 date = []
 methode = []
 page = []
-page_code = []
+status_code = []
 who = []
 
 for i, line in enumerate(open("/home/teun/Downloads/access.log")):
@@ -14,7 +20,19 @@ for i, line in enumerate(open("/home/teun/Downloads/access.log")):
        date.append(match.group(4))
        methode.append(match.group(5))
        page.append(match.group(6))
-       page_code.append(match.group(8))
+       status_code.append(match.group(8))
        who.append(match.group(11))
-       #.append(match.group(1))
-print(ip)
+       sql = "INSERT INTO `logs` (`ip`, `date`, `methode`, `page`, `status_code`, `who`) VALUES (%s, %s, %s, %s, %s, %s)"
+       cursor.execute(sql, (match.group(1), match.group(4), match.group(5), match.group(6), match.group(8), match.group(11)))
+       connection.commit()
+       
+
+
+# sql = "SHOW COLUMNS FROM `logs`"
+# cursor.execute(sql)
+
+# result = cursor.fetchall()
+# for i in result:
+#   print(i)
+
+
