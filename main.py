20c9163e1f3e2a7a24be5ce90b52574975c4f02f
Teun <teunteun@live.nl>
2020-11-30 13:57:18 +0100

    commented out all the thing that were out of use but still maybe relavant in newer versions

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ cursor = connection.cursor();
 
 
 pattern = re.compile("^(\S+) (\S+) (\S+) \[([\w:\/]+\s[+\-]\d{4})\] \"(\S+)\s?(\S+)?\s?(\S+)?\" (\d{3}|-) (\d+|-)\s?\"?([^\"]*)\"?\s?\"?([^\"]*)\"?\s")
-ip = []
-date = []
-methode = []
-page = []
-status_code = []
-who = []
+# ip = []
+# date = []
+# methode = []
+# page = []
+# status_code = []
+# who = []
 
 for i, line in enumerate(open("/home/teun/Downloads/access.log")):
     for match in re.finditer(pattern, line):
        # print 'Found on line %s: %s' % (i+1, match.group())
-       ip.append(match.group(1))
-       date.append(match.group(4))
-       methode.append(match.group(5))
-       page.append(match.group(6))
-       status_code.append(match.group(8))
-       who.append(match.group(11))
+       # ip.append(match.group(1))
+       # date.append(match.group(4))
+       # methode.append(match.group(5))
+       # page.append(match.group(6))
+       # status_code.append(match.group(8))
+       # who.append(match.group(11))
        sql = "INSERT INTO `logs` (`ip`, `date`, `methode`, `page`, `status_code`, `who`) VALUES (%s, %s, %s, %s, %s, %s)"
        cursor.execute(sql, (match.group(1), match.group(4), match.group(5), match.group(6), match.group(8), match.group(11)))
        connection.commit()
