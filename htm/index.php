a89f944a962ce4f173dd440f5fa12b9f5a22a1b2
Teun <teunteun@live.nl>
2020-12-09 15:29:04 +0100

    added google chart script

--- htm/index.php
+++ htm/index.php
@@ -10,50 +10,57 @@ if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT ip FROM logs";
+$sql = "SELECT date, status_code, ip FROM logs";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
+if ($result->num_rows < 1) {
+	while($row = $result->fetch_assoc()) {
                 echo "ip:" . $row["ip"]. "<br>";
-        }
+	};
 } else {
-        echo "0 results";
+        echo "more than 1 result";
 }
-$conn->close();
+# $conn->close();
 ?>
 
 <DOCTYPE html>
 <html>
         <header>
-	<script src="https://www.gstatic.com/charts/loader.js"></script>
-	  <script>
-	    google.charts.load('current', {packages: ['corechart']});
-	    google.charts.setOnLoadCallback(drawVisualization);
+        <script src="https://www.gstatic.com/charts/loader.js"></script>
+          <script>
+            google.charts.load('current', {packages: ['corechart']});
+            google.charts.setOnLoadCallback(drawVisualization);
 
-	    
-	      function drawVisualization() {
-	        // Some raw data (not necessarily accurate)
-	        var data = google.visualization.arrayToDataTable();
+	    function drawVisualization() {
+		    var data = google.visualization.arrayToDataTable([
+			      ['date', 'status_code'],
+<?php
+while($row = $result->fetch_assoc())
+{
+	echo "[".$row["status_code"].", ".$row["date"]."],";
+}
+echo "[0, 0]"
+?>
 
-	        var options = {
-	          title : 'Monthly Coffee Production by Country',
-	          vAxis: {title: 'Cups'},
-	          hAxis: {title: 'Month'},
-	          seriesType: 'bars',
-	          series: {5: {type: 'line'}}
-	        };
+]);
+                var options = {
+                  title : 'test',
+                  vAxis: {title: 'date'},
+                  hAxis: {title: 'status_code'},
+		  seriesType: 'bars',
+		  series: {5: {type: 'line'}}
+                };
 
 	        var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
 	        chart.draw(data, options);
-	      }
+	      } 
 	   </script>
 
         </header>
         <body>
         <div id="dashboard_div">
                 <div id="myPieChart"></div>
-                <div id="chart_div"></div>
+                <div id="chart_div" style="width: 900px; height:500px;"></div>
         </div>
         </body>
 </html>
